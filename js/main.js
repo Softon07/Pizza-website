3d7880da39c1abacbae30baf4345fafa54859838
Marcel <marcelnedza2000@gmail.com>
2023-01-25 19:00:24 +0100

    navigation for mobile

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const mobileAnchors = document.querySelector('.nav-mobile__anchors');
+const burgerBtn = document.querySelector('.burger-icon');
+const xIcon = document.querySelector('.fa-times');
+const barsIcon = document.querySelector('.fa-bars');
+
+const handleNav = () => {
+    mobileAnchors.classList.toggle('active-menu');
+    burgerBtn.classList.toggle('active-menu');
+    xIcon.classList.toggle('hide');
+    barsIcon.classList.toggle('hide');
+}
+
+burgerBtn.addEventListener('click', handleNav);
