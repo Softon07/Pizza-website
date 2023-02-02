ba69a9637bca7ba3568b447dabac71892772aab3
Marcel <marcelnedza2000@gmail.com>
2023-02-02 21:56:17 +0100

    menu section, pizza section made[HTML, CSS] , menu.js added(switch between sections in menu[pizza, sauce, promotion, drinks])

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+const sections = {
+    Pizza: '.pizza',
+    Sosy: '.sauce',
+    'Pizza Promocyjna': '.promotion',
+    Napoje: '.drinks'
+};
+
+const btns = document.querySelectorAll('.menu__list a');
+
+// change section
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        for (const section in sections) {
+            const el = document.querySelector(sections[section]);
+            el.classList.toggle('not-visible', section !== btn.textContent);
+        }
+    });
+});
+
