c5a6a17f81514005848e87d58c97f73f59347a39
Marcel <marcelnedza2000@gmail.com>
2023-02-02 12:44:54 +0100

    form opinion done

--- js/opinion.js
+++ js/opinion.js
@@ -2,15 +2,14 @@ const formBtn = document.querySelector('.form__btn');
 const yourName = document.querySelector('#name');
 const yourOpinion = document.querySelector('#testimonials');
 const clientsList = document.querySelector(".opinion__clients-list");
-
-
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const today = new Date().toLocaleDateString('pl-PL', options);
 const stars = document.querySelectorAll('.fa-star');
-let numOfStars = 0;
+let numOfStars = 5;
 
 stars.forEach((star, index) => {
   star.addEventListener('click', () => {
     numOfStars = index - 43;
-    console.log(numOfStars);
     for (let i = 0; i < stars.length; i++) {
       if (i > index) {
         stars[i].classList.remove('highlight');
@@ -21,71 +20,56 @@ stars.forEach((star, index) => {
   });
 });
 
+const removeStars = () => {
+  stars.forEach(star => star.classList.remove('highlight'));
+};
 
-
-
-
-
-const addOpinion = () => {
-  // Stworzenie kontenera dla opinii klienta i dodanie go do listy klientów
+const main = () => {
   const opinionContainer = document.createElement("div");
   opinionContainer.classList.add("opinion__client-rating");
   clientsList.appendChild(opinionContainer);
-
-  // Stworzenie kontenera dla gwiazdek oceny
+  
   const starsContainer = document.createElement("div");
   starsContainer.classList.add("opinion__stars");
 
-  // Stworzenie elementów gwiazdki
   for (let i = 0; i < numOfStars; i++) {
     const starElement = document.createElement("i");
     starElement.classList.add("fa-solid");
     starElement.classList.add("fa-star");
-    console.log(numOfStars);
     starsContainer.appendChild(starElement);
   }
-
-  // Stworzenie kontenera dla komentarza
+  
   const testimonialsContainer = document.createElement("div");
   testimonialsContainer.classList.add("opinion__testimonials");
 
-  // Stworzenie elementu tekstowego komentarza
   const testimonialsText = document.createElement("p");
   testimonialsText.textContent = `${yourOpinion.value}`;
   testimonialsContainer.appendChild(testimonialsText);
-
-  // Stworzenie linii separującej
+  
   const lineElement = document.createElement("div");
   lineElement.classList.add("opinion__line");
-
-  // Stworzenie kontenera dla autora komentarza
+  
   const testimonialsAuthorContainer = document.createElement("div");
   testimonialsAuthorContainer.classList.add("opinion__testimonials-author");
-
-  // Stworzenie elementu tekstowego autora komentarza
+  
   const testimonialsAuthorText = document.createElement("p");
-  testimonialsAuthorText.textContent = `${yourName.value}, 29 czerwca 2022`;
+  yourName.value = yourName.value || 'Gość';
+  testimonialsAuthorText.textContent = `${yourName.value}, ${today}`;
   testimonialsAuthorContainer.appendChild(testimonialsAuthorText);
-
-  // Stworzenie kontenera dla adnotacji
+  
   const annotationContainer = document.createElement("div");
   annotationContainer.classList.add("opinion__annotation");
-
-  // Stworzenie elementu tekstowego zawartości adnotacji
+  
   const annotationContent = document.createElement("p");
+  annotationContainer.appendChild(annotationContent);
   annotationContent.classList.add("content");
   annotationContent.textContent = "Dziękujemy za opinię";
-
-  // Stworzenie elementu tekstowego autora adnotacji
+  
   const annotationAuthor = document.createElement("p");
+  annotationContainer.appendChild(annotationAuthor);
   annotationAuthor.classList.add("author");
   annotationAuthor.textContent = "~ Zespół Pizzeriada";
-
-  // Dodanie elementów tekstowych adnotacji do kontenera
-  annotationContainer.appendChild(annotationContent);
-  annotationContainer.appendChild(annotationAuthor);
-
-  // Dodanie wszystkich stworzonych kontenerów i elementów do głównego kontenera
+  
   opinionContainer.appendChild(starsContainer);
   opinionContainer.appendChild(testimonialsContainer);
   opinionContainer.appendChild(lineElement);
@@ -94,8 +78,7 @@ const addOpinion = () => {
 
   yourName.value = "";
   yourOpinion.value = "";
-
+  removeStars();
 }
 
-
-formBtn.addEventListener('click', addOpinion);
+formBtn.addEventListener('click', main);
