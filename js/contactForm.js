6e6a0cafa943f895ae21860fd2ae7d6a71a9f63a
Marcel <marcelnedza2000@gmail.com>
2023-02-08 17:53:49 +0100

    continuing work on the form

--- js/contactForm.js
+++ js/contactForm.js
@@ -25,12 +25,12 @@ const checkErrors = () => {
     let errorCount = 0;
 
     allFormElements.forEach(el => {
-        if(el.classList.contains('error')) {
+        if (el.classList.contains('error')) {
             errorCount++;
         }
     })
 
-    if(errorCount === 0) {
+    if (errorCount === 0) {
         popup.classList.add('show-popup');
         cleanForm();
     }
@@ -38,13 +38,13 @@ const checkErrors = () => {
 
 const checkForm = input => {
     input.forEach(el => {
-        if(el.value === '') {
+        if (el.value === '') {
             showError(el);
-        } else { 
+        } else {
             clearError(el)
         }
     })
-} 
+}
 
 const cleanForm = () => {
     [username, surname, select1, select2, select3, textarea].forEach(el => {
@@ -63,3 +63,28 @@ closeBtn.addEventListener('click', e => {
     e.preventDefault();
     popup.classList.remove('show-popup');
 });
+
+
+// checkboxes
+const phone = document.querySelector('#phone');
+const email = document.querySelector('#email');
+const yesOrNo = document.querySelector('#yesOrNo');
+const checkboxes = document.querySelectorAll('.form__contact-checkbox--container input');
+
+
+checkboxes.forEach(el => {
+    el.addEventListener('click', () => {
+        if(el.checked && el.id === 'yesOrNo') {
+            email.checked = false;
+            phone.checked = false;
+            console.log(el.checked, el.id);
+        } else if (el.checked && el.id === 'phone') {
+            console.log(el.checked, el.id);
+            yesOrNo.checked = false;
+
+        } else if (el.checked && el.id === 'email') {
+            console.log(el.checked, el.id);
+            yesOrNo.checked = false;
+        }
+    })
+})
