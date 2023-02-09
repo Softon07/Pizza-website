c428a65c5522e83fc62e84b0c80c5e1a4c04b6f3
Marcel <marcelnedza2000@gmail.com>
2023-02-09 23:00:10 +0100

    checkboxes DOM added

--- js/contactForm.js
+++ js/contactForm.js
@@ -9,6 +9,12 @@ const popup = document.querySelector('.popup');
 const sendBtn = document.querySelector('.form__btn-inside');
 const closeBtn = document.querySelector('#closeBtn');
 
+const phone = document.querySelector('#phone');
+const email = document.querySelector('#email');
+const yesOrNo = document.querySelector('#yesOrNo');
+const checkboxes = document.querySelectorAll('.form__contact-checkbox--container input');
+const newPhone = document.querySelector('.new-phone');
+const newEmail = document.querySelector('.new-email');
 
 const showError = input => {
     const row = input.parentElement;
@@ -54,7 +60,6 @@ const cleanForm = () => {
 
 sendBtn.addEventListener('click', e => {
     e.preventDefault();
-
     checkForm([username, surname, select1, select2, select3, textarea]);
     checkErrors();
 });
@@ -64,27 +69,24 @@ closeBtn.addEventListener('click', e => {
     popup.classList.remove('show-popup');
 });
 
-
-// checkboxes
-const phone = document.querySelector('#phone');
-const email = document.querySelector('#email');
-const yesOrNo = document.querySelector('#yesOrNo');
-const checkboxes = document.querySelectorAll('.form__contact-checkbox--container input');
-
-
-checkboxes.forEach(el => {
+  checkboxes.forEach(el => {
     el.addEventListener('click', () => {
-        if(el.checked && el.id === 'yesOrNo') {
+        if (el.checked && el.id === 'yesOrNo') {
             email.checked = false;
             phone.checked = false;
-            console.log(el.checked, el.id);
+            newPhone.classList.remove('show-container');
+            newEmail.classList.remove('show-container');
         } else if (el.checked && el.id === 'phone') {
-            console.log(el.checked, el.id);
             yesOrNo.checked = false;
-
+            newPhone.classList.toggle('show-container');
         } else if (el.checked && el.id === 'email') {
-            console.log(el.checked, el.id);
             yesOrNo.checked = false;
+            newEmail.classList.toggle('show-container');
         }
-    })
-})
+        if (!el.checked && el.id === 'phone') {
+            newPhone.classList.remove('show-container');
+        } else if (!el.checked && el.id === 'email') {
+            newEmail.classList.remove('show-container');
+        }
+    });
+});
