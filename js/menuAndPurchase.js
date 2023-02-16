465c50ce45f5ec8437d887670af1f46880ec13c8
Marcel <marcelnedza2000@gmail.com>
2023-02-16 15:58:40 +0100

    modal pizza begin

--- js/menuAndPurchase.js
+++ js/menuAndPurchase.js
@@ -16,3 +16,32 @@ btns.forEach(btn => {
     });
 });
 
+const modal = document.querySelector('.modal');
+const pizzaNameElement = document.querySelector('.modal-item .pizza-name');
+const closeModalBtn = document.querySelector('#closeModalBtn');
+
+const showModal = () => {
+    modal.classList.add('show-modal');
+}
+
+// pressed button
+const pizzaBtns = document.querySelectorAll('.element__btn');
+pizzaBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+        // modal has: pizza's name
+        const pizzaName = btn.closest('div').querySelector('p').textContent;
+        console.log(pizzaName);
+
+        pizzaNameElement.textContent = pizzaName;
+        // show modal
+        showModal();
+
+    });
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.remove('show-modal');
+})
+
+
