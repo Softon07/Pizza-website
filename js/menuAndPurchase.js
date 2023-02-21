5141b5972dfc064bf3549a1c68a80b3ad674cfaa
Marcel <marcelnedza2000@gmail.com>
2023-02-21 18:53:26 +0100

    record of work on modals in the menu

--- js/menuAndPurchase.js
+++ js/menuAndPurchase.js
@@ -9,6 +9,11 @@ const pizza = document.querySelector('.pizza');
 const sauce = document.querySelector('.sauce');
 const drinks = document.querySelector('.drinks');
 
+const modalOverlay = document.querySelector('.modal-overlay');
+
+let productsCounter = document.querySelector('.shopping-cart .counter');
+productsCounter.textContent = 0;
+
 // change section
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -21,9 +26,11 @@ btns.forEach(btn => {
 
 const modalPizza = document.querySelector('.modal-pizza');
 const modalSauce = document.querySelector('.modal-sauce');
-const modalDrinks = document.querySelector('.modal-drinks');
+const modalDrinks = document.querySelector('.modal-cart');
+const modalCart = document.querySelector('.modal-cart');
 
 const menuElement = document.querySelectorAll('.modal .el-name');
+const pizzaIngredients = document.querySelectorAll('.pizza-ingredients .ingredients');
 const closeModalBtn = document.querySelectorAll('#closeModalBtn');
 
 const showModal = () => {
@@ -34,32 +41,50 @@ const showModal = () => {
     } else if (!drinks.classList.contains('not-visible')) {
         modalDrinks.classList.add('show-modal');
     }
+    modalOverlay.style.display = 'block';
+    document.body.style.overflow = 'hidden';
 }
+
+let pizzaName, pizzaComponents, price;
 
 // pressed button
 const elementsBtns = document.querySelectorAll('.element__btn');
 elementsBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        
-        const nameElement = btn.closest('div').querySelector('p').textContent;
-        console.log(nameElement);
 
+        // const pizzaSize = ;
+
+        const nameElement = btn.closest('div').querySelector('p').textContent;
         // dynamiczne nadanie nazwy na samej górze na modalu do przedmiotu który się kupuje.
         menuElement.forEach(el => {
             el.textContent = nameElement;
+            pizzaName = nameElement;
         });
 
-        // show modal
+        // only for pizza category
+        try {
+            const componentsElement = btn.closest('.pizza__list').querySelector('.pizza__components p');
+            const components = componentsElement.textContent;
+            pizzaIngredients.forEach(el => {
+                el.textContent = components;
+                pizzaComponents = components;
+            });
+        } catch (error) {
+            console.error('Product outside the pizza category!');
+        }
         showModal();
-
     });
 });
+
+
 
 const closeModal = () => {
     modalPizza.classList.remove('show-modal');
     modalSauce.classList.remove('show-modal');
     modalDrinks.classList.remove('show-modal');
-    console.log('wylaczono modal');
+    modalCart.classList.remove('show-modal');
+    modalOverlay.style.display = 'none';
+    document.body.style.overflow = 'auto';
 }
 
 closeModalBtn.forEach(el => {
@@ -69,28 +94,39 @@ closeModalBtn.forEach(el => {
     });
 });
 
-const small = document.querySelector('#small');
-const medium = document.querySelector('#medium');
-const big = document.querySelector('#big');
+// pizza inputs
+const smallPizza = document.querySelector('#small-pizza');
+const mediumPizza = document.querySelector('#medium-pizza');
+const bigPizza = document.querySelector('#big-pizza');
 const flat = document.querySelector('#flat');
 const thick = document.querySelector('#thick');
 
+// sauce inputs
+const smallSauce = document.querySelector('#small-sauce');
+const mediumSauce = document.querySelector('#medium-sauce');
+
+
+// drinks inputs
+const smallDrinks = document.querySelector('#small-drinks');
+const mediumDrinks = document.querySelector('#medium-drinks');
+const bigDrinks = document.querySelector('#big-drinks');
+
 const cleanModal = () => {
-    [small, medium, big, flat, thick].forEach(el => {
+    [smallPizza, mediumPizza, bigPizza, flat, thick, smallSauce, mediumSauce, smallDrinks, mediumDrinks, bigDrinks].forEach(el => {
         el.checked = false;
     });
 }
 
-const handleRadio = (el, small, medium, big, flat, thick) => {
-    if (el.checked && el === small) {
-        medium.checked = false;
-        big.checked = false;
-    } else if (el.checked && el === medium) {
-        small.checked = false;
-        big.checked = false;
-    } else if (el.checked && el === big) {
-        small.checked = false;
-        medium.checked = false;
+const handleRadio = (el, smallPizza, mediumPizza, bigPizza, flat, thick, smallSauce, mediumSauce, smallDrinks, mediumDrinks, bigDrinks) => {
+    if (el.checked && el === smallPizza) {
+        mediumPizza.checked = false;
+        bigPizza.checked = false;
+    } else if (el.checked && el === mediumPizza) {
+        smallPizza.checked = false;
+        bigPizza.checked = false;
+    } else if (el.checked && el === bigPizza) {
+        smallPizza.checked = false;
+        mediumPizza.checked = false;
     }
 
     if (el.checked && el === flat) {
@@ -98,22 +134,98 @@ const handleRadio = (el, small, medium, big, flat, thick) => {
     } else if (el.checked && el === thick) {
         flat.checked = false;
     }
+
+    if (el.checked && el === smallSauce) {
+        mediumSauce.checked = false;
+    } else if (el.checked && el === mediumSauce) {
+        smallSauce.checked = false;
+    }
+
+    if (el.checked && el === smallDrinks) {
+        mediumDrinks.checked = false;
+        bigDrinks.checked = false;
+    } else if (el.checked && el === mediumDrinks) {
+        smallDrinks.checked = false;
+        bigDrinks.checked = false;
+    } else if (el.checked && el === bigDrinks) {
+        smallDrinks.checked = false;
+        mediumDrinks.checked = false;
+    }
 }
 
-[small, medium, big, flat, thick].forEach(el => {
+[smallPizza, mediumPizza, bigPizza, flat, thick, smallSauce, mediumSauce, smallDrinks, mediumDrinks, bigDrinks].forEach(el => {
     el.addEventListener('click', () => {
-        handleRadio(el, small, medium, big, flat, thick);
+        handleRadio(el, smallPizza, mediumPizza, bigPizza, flat, thick, smallSauce, mediumSauce, smallDrinks, mediumDrinks, bigDrinks);
     });
 });
+
+const emptyCart = document.querySelector('.cart-empty');
+
+const showCartModal = () => {
+    modalCart.classList.add('show-modal');
+    modalOverlay.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+
+    if (productsCounter.textContent > 0) {
+        emptyCart.style.display = "none";
+        console.log(productsCounter.textContent);
+    } else {
+        emptyCart.style.display = "block";
+        console.log(productsCounter.textContent);
+    }
+
+}
+
+
+const addItemToCart = (pizzaName, pizzaComponents) => {
+    productsCounter.textContent++;
+
+    const productList = document.querySelector('.products-list');
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('product');
+    const productName = document.createElement('p');
+    productName.classList.add('product-name');
+    
+    productName.textContent = pizzaName;
+    
+    const productDetails = document.createElement('p');
+    productDetails.classList.add('product-details');
+    productDetails.textContent = pizzaComponents;
+    
+    const lineDiv = document.createElement('div');
+    lineDiv.classList.add('line');
+    
+    productDiv.appendChild(productName);
+    productDiv.appendChild(productDetails);
+    productDiv.appendChild(lineDiv);
+    
+    productList.appendChild(productDiv);
+
+}
 
 // add item to shooping-cart
 const addItemBtn = document.querySelectorAll('#addItemBtn');
 
 addItemBtn.forEach(el => {
     el.addEventListener('click', () => {
-        console.log('dodano do koszyka');
         closeModal();
         cleanModal();
-        // addItemToCart();
+        addItemToCart(pizzaName, pizzaComponents);
     });
+});
+
+// clicking outside the modal disables the modal
+document.addEventListener('click', (event) => {
+    if (event.target === modalOverlay) {
+        closeModal();
+    }
+});
+
+
+// show CardModal
+
+const cardBtn = document.querySelector('.shopping-cart');
+
+cardBtn.addEventListener('click', () => {
+    showCartModal();
 });
--- js/opinion.js
+++ js/opinion.js
@@ -28,7 +28,7 @@ const main = () => {
   const opinionContainer = document.createElement("div");
   opinionContainer.classList.add("opinion__client-rating");
   clientsList.appendChild(opinionContainer);
-  
+
   const starsContainer = document.createElement("div");
   starsContainer.classList.add("opinion__stars");
 
@@ -38,38 +38,38 @@ const main = () => {
     starElement.classList.add("fa-star");
     starsContainer.appendChild(starElement);
   }
-  
+
   const testimonialsContainer = document.createElement("div");
   testimonialsContainer.classList.add("opinion__testimonials");
 
   const testimonialsText = document.createElement("p");
   testimonialsText.textContent = `${yourOpinion.value}`;
   testimonialsContainer.appendChild(testimonialsText);
-  
+
   const lineElement = document.createElement("div");
   lineElement.classList.add("opinion__line");
-  
+
   const testimonialsAuthorContainer = document.createElement("div");
   testimonialsAuthorContainer.classList.add("opinion__testimonials-author");
-  
+
   const testimonialsAuthorText = document.createElement("p");
   yourName.value = yourName.value || 'Gość';
   testimonialsAuthorText.textContent = `${yourName.value}, ${today}`;
   testimonialsAuthorContainer.appendChild(testimonialsAuthorText);
-  
+
   const annotationContainer = document.createElement("div");
   annotationContainer.classList.add("opinion__annotation");
-  
+
   const annotationContent = document.createElement("p");
   annotationContainer.appendChild(annotationContent);
   annotationContent.classList.add("content");
   annotationContent.textContent = "Dziękujemy za opinię";
-  
+
   const annotationAuthor = document.createElement("p");
   annotationContainer.appendChild(annotationAuthor);
   annotationAuthor.classList.add("author");
   annotationAuthor.textContent = "~ Zespół Pizzeriada";
-  
+
   opinionContainer.appendChild(starsContainer);
   opinionContainer.appendChild(testimonialsContainer);
   opinionContainer.appendChild(lineElement);
